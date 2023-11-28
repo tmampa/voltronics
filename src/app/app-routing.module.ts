ebd371bf5805f24e3b42aec1661d26e189c9c453
tmampa <tmampa94@gmail.com>
2023-11-28 15:28:58 +0200

    Protect routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent, RegisterComponent, LoginComponent, CartComponent } from "./_pages";
+import { AuthGuard } from "@app/_helpers";
 
 const routes: Routes = [
-  { path: '', component : HomeComponent },
+  { path: '', component : HomeComponent, canActivate: [AuthGuard] },
   { path: 'account/register', component : RegisterComponent },
   { path: 'account/login', component : LoginComponent },
   { path: 'cart', component : CartComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { fakeBackendProvider } from './_helpers';
 
 import { AppComponent } from './app.component';
 import { HomeComponent, LoginComponent, RegisterComponent, CartComponent } from './_pages';
@@ -17,9 +21,13 @@ import { NavbarComponent } from './_components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
