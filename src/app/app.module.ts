ed1335dc33d9997bdc1961c4e2ead4696f4fe2c4
tmampa <tmampa94@gmail.com>
2023-11-28 08:21:54 +0200

    Create files and folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './_pages/home/home.component';
+import { LoginComponent } from './_pages/login/login.component';
+import { RegisterComponent } from './_pages/register/register.component';
+import { CartComponent } from './_pages/cart/cart.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    CartComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
