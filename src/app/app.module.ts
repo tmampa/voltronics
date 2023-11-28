60f5c368b34a7c4c781937f0fdcf13d09ea624ff
tmampa <tmampa94@gmail.com>
2023-11-28 19:54:15 +0200

    Login Form, Authentication & Route Guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { fakeBackendProvider } from './_helpers';
+import {ErrorInterceptor, fakeBackendProvider, JwtInterceptor} from './_helpers';
 
 import { AppComponent } from './app.component';
 import { HomeComponent, LoginComponent, RegisterComponent, CartComponent } from './_pages';
 import { NavbarComponent } from './_components/navbar/navbar.component';
+import { AlertComponent } from './_components/alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { NavbarComponent } from './_components/navbar/navbar.component';
     LoginComponent,
     RegisterComponent,
     CartComponent,
-    NavbarComponent
+    NavbarComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +28,8 @@ import { NavbarComponent } from './_components/navbar/navbar.component';
     HttpClientModule
   ],
   providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     fakeBackendProvider
   ],
   bootstrap: [AppComponent]
