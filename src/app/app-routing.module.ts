2227f80f4904c71677eb1c647e8b748abc2cacc2
tmampa <tmampa94@gmail.com>
2023-11-28 08:37:33 +0200

    Add routing between components and a navbar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent, RegisterComponent, LoginComponent, CartComponent } from "./_pages";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component : HomeComponent },
+  { path: 'account/register', component : RegisterComponent },
+  { path: 'account/login', component : LoginComponent },
+  { path: 'cart', component : CartComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './_pages/home/home.component';
-import { LoginComponent } from './_pages/login/login.component';
-import { RegisterComponent } from './_pages/register/register.component';
-import { CartComponent } from './_pages/cart/cart.component';
+import { HomeComponent, LoginComponent, RegisterComponent, CartComponent } from './_pages';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 
 @NgModule({
