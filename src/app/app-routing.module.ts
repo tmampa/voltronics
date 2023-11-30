1a167b0d2e1ed36d3ea439689809d5948e216b90
tmampa <tmampa94@gmail.com>
2023-11-30 13:47:48 +0200

    project pages with auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,15 @@ import { AuthGuard } from "@app/_helpers";
 import {ElectronicComponent} from "@app/_pages/electronic/electronic.component";
 import {JeweleryComponent} from "@app/_pages/jewelery/jewelery.component";
 import {MenComponent} from "@app/_pages/men/men.component";
-import {WomenComponent} from "@app/_pages/women/women.component";
 
 const usersModule = () => import('./_pages/users/users.module').then(x => x.UsersModule);
 
 const routes: Routes = [
   { path: '', component : HomeComponent, canActivate: [AuthGuard] },
-  { path: 'electronics', component : ElectronicComponent },
-  { path: 'jewelery', component : JeweleryComponent },
-  { path: 'mens', component: MenComponent },
-  { path: 'womens', component: WomenComponent },
+  { path: 'electronics', component : ElectronicComponent, canActivate: [AuthGuard] },
+  { path: 'jewelery', component : JeweleryComponent, canActivate: [AuthGuard] },
+  { path: 'mens', component: MenComponent, canActivate: [AuthGuard] },
+  { path: 'womens', component: MenComponent, canActivate: [AuthGuard] },
   { path: 'users', loadChildren: usersModule, canActivate: [AuthGuard] },
   { path: 'account/register', component : RegisterComponent },
   { path: 'account/login', component : LoginComponent },
